07e51326449c9b3e60bbf9161a6c749cc40221b1
mattes1784 <baumannmatthias@web.de>
2016-01-03 14:31:05 +0100

    Last changes on this new branch

--- main.c
+++ main.c
@@ -32,15 +32,13 @@ int main()
     uint32_t iterations;
     for(iterations = 0; iterations < 500000; iterations++)
     {
-        //clear_screen();
-        //printf("Iterationcounter: %d\n", iterations);
+        clear_screen();
+        printf("Iterationcounter: %d\n", iterations);
         update_field( field , sizeof(field) , sizeof(field[0]), &last_changed_position);
-        //print_fields( field , sizeof(field) , sizeof(field[0]) );
-        //usleep(30000); // calculated everything with 20 Hz
+        print_fields( field , sizeof(field) , sizeof(field[0]) );
+        usleep(100000); // calculated everything with 10 Hz
     }
-    clear_screen();
-    printf("Iterationcounter: %d\n", iterations);
-    print_fields( field , sizeof(field) , sizeof(field[0]) );
+
     printf("x_position lcp:%d\ny_position lcp:%d", last_changed_position.x, last_changed_position.y);
 
     return 0;
